94ba6cd9da9a92a0b942c0e154d27ce1c9c053cc
NeWive <738767136@QQ.com>
2019-08-30 15:06:59 +0800

    init

---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+import './Test.scss'
+class Test extends PureComponent {
+    render() {
+        return(
+            <div id={'test'}>
+                123
+            </div>
+        )
+    }
+}
+
+export default Test;
